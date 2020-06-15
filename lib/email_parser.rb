28e11ddd6d0b3cdc24946cfa96a308430d1968a6
Kevin C Malin <racmalin@yahoo.com>
2020-06-15 17:29:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,9 +1,14 @@
- class EmailAddressParser 
- 
-# attr_accessor :name, :csv_emails
- 
- def initialize(csv_emails)
-  # @name = name
-   @csv_emails = csv_emails
+ class EmailParser
+
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
   end
- end
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+end
